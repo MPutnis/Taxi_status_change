405d0af242ba523e49e5cf0458f0cfabd3107c83
MPutnis <martins.putnis@gmail.com>
2025-02-28 06:59:55 +0200

    Taxi status change function

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Task list:
+
+/* 1. Status change function
+        1a. Input data: Firm ID, Taxi ID, Role ID, NewStatus ID
+        1b. Output: Change Taxi Status and Timestamp or deny any changes
+        1c. Retrieve Current status id and timestamp, compare  if 
+            status IDs are different, proceed if yes, else end function
+        1d. Determine Reole of Change initializer
+        1e. If change is logical, then make change, else no changes
+        1f. Maybe add a check to see if taxi belongs to firm
+*/
+/* 2. Data lists 
+    
+
+*/
